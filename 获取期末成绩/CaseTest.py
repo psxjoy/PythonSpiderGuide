54e0897e458a5e37c9bc4d4cd0e15a08813b2667
Pan shuxin <psxjoy@outlook.com>
2016-06-16 21:09:55 +0800

    DailyAdd
    本次修改问题：
    1.再次修改编码问题
    2.再次提升程序的完全性

--- 获取期末成绩/CaseTest.py
+++ 获取期末成绩/CaseTest.py
@@ -1,28 +1,5 @@
-import json
-import requests
-from email import encoders
-from email.header import Header
-from email.mime.text import MIMEText
-from email.utils import parseaddr, formataddr
-import smtplib
-import time
+checklist = ['美丽科学']
+l = 'jACK'
 
-url = 'http://112.124.54.19/Score/score/importScoreFromSchool.action'
-header = {
-    'User-Agent':'Mozilla/5.0 (Linux; Android 6.0.1; MI 4LTE Build/MMB29M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/50.0.2661.86 Mobile Safari/537.36-SuperFriday_7.4.1',
-    'Cookie':'ACNZZDATA1252894711=89097748-1465629103-%7C1465629103',
-    'Content-Type':'application/x-www-form-urlencoded'
-}
-data = {
-    'schoolIdentity':'7D6F7B500C3C8AE89C0ED9362898CD0F',
-    's_Id':'0',
-    'b_y':'2015',
-    't_m':'2',
-    's_n':'XXX',
-    'p_d':'XXX',
-    'c_k':'',
-    'v_c':'',
-    'identity':'E0C90E6559BB6E23212F197976DE9A1B'
-}
-wb_data = requests.post(url,headers=header,data=data)
-print(wb_data.text)
+checklist.append(l)
+print(checklist)
--- 获取期末成绩/main.py
+++ 获取期末成绩/main.py
@@ -9,33 +9,35 @@ import time
 
 url = 'http://112.124.54.19/Score/score/importScoreFromSchool.action'
 header = {
-    'User-Agent':'Mozilla/5.0 (Linux; Android 6.0.1; MI 4LTE Build/MMB29M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/50.0.2661.86 Mobile Safari/537.36-SuperFriday_7.4.1',
-    'Cookie':'ACNZZDATA1252894711=89097748-1465629103-%7C1465629103',
-    'Content-Type':'application/x-www-form-urlencoded'
+    'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0.1; MI 4LTE Build/MMB29M; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/50.0.2661.86 Mobile Safari/537.36-SuperFriday_7.4.1',
+    'Cookie': 'ACNZZDATA1252894711=89097748-1465629103-%7C1465629103',
+    'Content-Type': 'application/x-www-form-urlencoded'
 }
 data = {
-    'schoolIdentity':'7D6F7B500C3C8AE89C0ED9362898CD0F',
-    's_Id':'0',
-    'b_y':'2015',
-    't_m':'2',
-    's_n':'XXX',  # 这里输入你的学号
-    'p_d':'XXXX',  # 这里输入你的密码
-    'c_k':'',
-    'v_c':'',
-    'identity':'E0C90E6559BB6E23212F197976DE9A1B'
+    'schoolIdentity': '7D6F7B500C3C8AE89C0ED9362898CD0F',
+    's_Id': '0',
+    'b_y': '2015',
+    't_m': '2',
+    's_n': 'xxx',  # 这里输入你的学号
+    'p_d': 'xxx',  # 这里输入你的密码
+    'c_k': '',
+    'v_c': '',
+    'identity': 'E0C90E6559BB6E23212F197976DE9A1D'
 }
+
+
 def _format_addr(s):
     name, addr = parseaddr(s)
     return formataddr((Header(name, 'utf-8').encode(), addr))
 
 
 def send_message(meg):
-    form_addr = 'XXX@XXX.com'  # 这里输入你的邮箱地址
-    password = 'XXXXX'  # 这里输入你的邮箱密码
-    to_addr = 'XXXX@XXX.com'  # 这里输入你的手机邮箱地址
-    smtp_server = 'smtp.XXXX.com'  # 这里输入你邮箱的smtp地址
+    form_addr = 'xxx@xxxx.com'
+    password = 'xxx'
+    to_addr = 'xxxxx@139.com'
+    smtp_server = 'xxx.xxx.com'
 
-    msg = MIMEText(meg,'plain','utf-8')
+    msg = MIMEText(meg, 'plain', 'utf-8')
     msg['From'] = _format_addr('成绩提醒<%s>' % form_addr)
     msg['To'] = _format_addr(' <%s>' % to_addr)
     msg['Subject'] = Header('期末成绩', 'utf-8').encode()
@@ -46,20 +48,26 @@ def send_message(meg):
     server.sendmail(form_addr, [to_addr], msg.as_string())
     server.quit()
 
-check =['魅力科学']
+
+check = ['魅力科学']
 while True:
-    wb_data = requests.post(url,headers=header,data=data)
+    wb_data = requests.post(url, headers=header, data=data)
     GetList = json.loads(wb_data.text)
-    if GetList['errorCode']==0:
+    if GetList['errorCode'] == 0:
         LengthList = len(GetList['scoreList'])
-        for i in range(0,LengthList):
+        for i in range(0, LengthList):
             CheckList = GetList['scoreList'][i]['courseName']
-            if CheckList not  in check:
-                meg=GetList['scoreList'][i]['courseName']+GetList['scoreList'][i]['courseProperty']+GetList['scoreList'][i]['score']
+            if CheckList not in check:
+                Subject = str(GetList['scoreList'][i]['courseName'])
+                Theme = str(GetList['scoreList'][i]['courseProperty'])
+                Score = str(GetList['scoreList'][i]['score'])
+                meg = '您的' + Subject + '(' + Theme + ')成绩已经公布，成绩为' + Score
                 send_message(meg)
-                print(meg)
+                # print(type(meg))
                 check.append(CheckList)
     else:
-        print('cuowula')
+        meg1 = str(GetList['message'])
+        send_message(meg1)
+
         break
     time.sleep(180)
